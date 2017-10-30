0b1f5108ba79a32233c8db954267cae9afd7e5ab
Gavin <31544961+GucciGavin@users.noreply.github.com>
2017-10-30 08:28:46 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<title>	Gavin Conatser.com </title>
+<style>
+body {
+	background-image: url("img/supreme-beach.jpg");
+}
+	
+h1 {
+	font-family: Sans-serif;
+	font-size: 400%;
+	text-align: center;
+	color: white;
+	text-shadow: 3px 2px black;
+}
+
+a {
+	color: white;
+	text-decoration: none;
+}
+
+a:hover {
+	color: red;
+	background-color: white;
+	text-decoration: none;
+}
+
+h2 {
+	text-align: center;
+	color: white;
+}
+
+img:hover {
+    filter: alpha(opacity=90);
+    opacity: .6;
+}
+
+footer {
+	font-size: 150%;
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	center;
+	color: black;
+}
+}
+
+h3 {
+	text-align: center;
+}
+
+</style>
+</head>
+<body style="CURSOR: url('cursor.ani')">
+<h1>Gavin's MainHub</h1>
+<h2>
+<a href = "html/">HTML</a>
+<br />
+<a href = "python/">PYTHON</a>
+<br />
+<a href = "http://www.supremenewyork.com/shop/all">SUPREME</a>
+<br />
+<a href = "https://github.com/GucciGavin" >GITHUB</a>
+<br />
+<a href = "mdb/" >MDB</a>
+<br />
+<a href = "php/">PHP</a>
+<br />
+<a href="https://www.grailed.com/">GRAILED</a>
+<br />
+<a href="https://www.flightclub.com/">FLIGHTCLUB</a>
+<br />
+<a href="https://us.bape.com/collections/frontpage">BAPE</a>
+<br />
+</h2>
+<footer>
+Contact: gavinc.cs@tritechsc.org
+
+</footer>
+</body>
+</html>
